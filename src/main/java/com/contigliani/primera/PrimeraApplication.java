5a131bb1da0c0278aae20db7837dd37385625c6e
nico.contigliani <ncontigliani@gdpsalud.com.ar>
2021-04-08 12:30:05 -0300

    primero

---
+++ src/main/java/com/contigliani/primera/PrimeraApplication.java
@@ -0,0 +1,13 @@
+package com.contigliani.primera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contigliani/primera/PrimeraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contigliani.primera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
